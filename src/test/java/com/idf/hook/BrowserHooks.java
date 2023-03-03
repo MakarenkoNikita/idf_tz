d910a5130e89d2ed1b51ef2bc517a0646779a29a
Makarenko Nikita <makarenkonv99@gmail.com>
2023-03-03 18:06:06 +0300

    fix

--- src/test/java/com/idf/hook/BrowserHooks.java
+++ src/test/java/com/idf/hook/BrowserHooks.java
@@ -1,6 +1,5 @@
 package com.idf.hook;
 
-import framework.utilities.RandomUtility;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
@@ -14,7 +13,6 @@ public class BrowserHooks {
 
     @BeforeTest
     public void configBrowser() {
-        RandomUtility.getRandomCorrectPassword();
         Configuration.readConfigFile();
         getBrowser().setTimeoutPageLoad(Configuration.getTimeoutPageLoad());
         getBrowser().setImplicitWait(Configuration.getImplicitWait());
